7b67e9b449dcc4a323a4746e9f770b3b5ff83a25
muroslav2909 <myroslav@preply.com>
2016-10-09 03:56:25 +0300

    c-list

--- rentbestcar/urls.py
+++ rentbestcar/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from settings import STATIC_ROOT
 from tastypie.api import Api
 from app.api import *
+from django.views.generic import RedirectView
 
 v1_api = Api(api_name='v1')
 v1_api.register(UserResource())
@@ -15,12 +16,12 @@ v1_api.register(TransactionResource())
 urlpatterns = [
     url(r'^$', 'app.views.index', name='index'),
     url(r'^car/(?P<car_id>\d+)/$', 'app.views.car', name='car'),
-    url(r'^car-edit/(?P<id>[0-9]+)/$', 'app.views.car_edit', name='car_edit'),
-    url(r'^car-list$', 'app.views.car_list', name='car_list'),
-    url(r'^owner-profile$', 'app.views.owner_profile', name='owner_profile'),
-    url(r'^owner-profile-edit$', 'app.views.owner_profile_edit', name='owner_profile_edit'),
-    url(r'^renter-profile$', 'app.views.renter_profile', name='renter_profile'),
-    url(r'^renter-profile-edit$', 'app.views.renter_profile_edit', name='renter_profile_edit'),
+    url(r'^car_edit/(?P<id>[0-9]+)/$', 'app.views.car_edit', name='car_edit'),
+    url(r'^car_list', 'app.views.car_list', name='car_list'),
+    url(r'^owner_profile$', 'app.views.owner_profile', name='owner_profile'),
+    url(r'^owner_profile_edit$', 'app.views.owner_profile_edit', name='owner_profile_edit'),
+    url(r'^renter_profile$', 'app.views.renter_profile', name='renter_profile'),
+    url(r'^renter_profile_edit$', 'app.views.renter_profile_edit', name='renter_profile_edit'),
     url(r'^transaction$', 'app.views.transaction', name='transaction'),
 
     # auth
@@ -30,6 +31,8 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(v1_api.urls)),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/img/landing/22222.png'),
+        )
 ]
 
 urlpatterns += patterns('', (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': STATIC_ROOT}), )
